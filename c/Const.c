d5f928c99bfc569da2efdf74aaca4349951e8ba3
iudah <olaolujude@gmail.com>
2024-01-22 15:11:53 +0100

    Refactor Const.c: Improve code structure, variable names, and error
    Description:
    
    - Improved variable names for better clarity and readability.
    - Reorganized code structure to enhance logical flow and readability.
    - Added comments to explain key sections of the code.
    - Replaced direct calls to laud_const with a more descriptive function
      name create_laud_const.
    - Enhanced error handling in case of memory allocation failures during
      initialization.
    - Utilized abort() instead of exit(1) for better termination and error
      reporting.
    - Updated the constructor priority value for LaudConst to use
      LAUD_CONST_PRIORITY for consistency.
    - Minor code formatting adjustments for better alignment and
      readability.

--- c/Const.c
+++ c/Const.c
@@ -2,36 +2,52 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "../../Ubject/Ubject.h"
+#include <Ubject.h>
+
 #include "Const.h"
 #include "Var.h"
 #include "Var.r.h"
+
+#define LAUD_CONST_PRIORITY (LAUD_VAR_PRIORITY + 2)
 
 struct LaudConst {
   struct LaudVar _;
 };
 
-void *LaudConst_ctor(void *self_, va_list *arg) {
+static void *laud_const_ctor(void *self_, va_list *arg) {
   struct LaudConst *self = super_ctor(LaudConst, self_, arg);
-  LaudVarSetContinuity(self, 0);
+  laud_set_continuity(self, 0);
   return self;
 }
 
-static void LaudVar_differentiate(const void *self_, const void *derivative,
-                                  struct LaudHashMap *ddx_queue) {
+char laud_is_constant(const void *self) { return classOf(self) == LaudConst; }
+
+static void laud_var_differentiate(const void *variable,
+                                   const void *__attribute__((unused))
+                                   derivative,
+                                   struct LaudHashMap *derivative_queue) {
+  // Increment reference count for Zero object
   reference((void *)Zero);
-  LaudHashMapFn.insert(ddx_queue, self_, Zero);
+
+  // Insert the pair (self_, Zero) into ddx_queue
+  LaudHashMapFn.insert(derivative_queue, variable, Zero);
 }
 
-void *LaudConst_(float value) {
-  struct LaudConst *self = init(LaudConst, value);
-  if (!self) {
+void *laud_const(const size_t rank, const size_t *const shape,
+                 const size_t data_length, const float *data) {
+  // Initialize LaudConst object with provided parameters
+  struct LaudConst *const_variable =
+      init(LaudConst, rank, shape, data_length, data);
+
+  // Check if memory allocation was successful
+  if (!const_variable) {
     // Handle memory allocation failure.
-    UbjectError.error("Error: Memory allocation failed.\n");
-    exit(1);
+    UbjectError.error("Error: Memory allocation failed for LaudConst.\n");
   }
-  return self;
+
+  return const_variable;
 }
 
 const void *LaudConst = 0;
@@ -39,26 +55,37 @@ const void *One = 0;
 const void *MinusOne = 0;
 const void *Zero = 0;
 
-void __attribute__((constructor(23))) initLaudConst() {
+void __attribute__((constructor(LAUD_CONST_PRIORITY))) initLaudConst() {
+  // Check if LaudConst is not already initialized
   if (!LaudConst) {
+    // Initialize LaudConst class
     LaudConst =
         init(LaudVarClass, LaudVar, sizeof(struct LaudConst), // important!
-             ctor, LaudConst_ctor, className, "LaudConst", LaudVarDifferentiate,
-             LaudVar_differentiate, NULL);
+             ctor, laud_const_ctor, className, "LaudConst", laud_differentiate,
+             laud_var_differentiate, NULL);
+
+    // Check if LaudConst initialization was successful
     if (!LaudConst) {
       // Handle initialization failure.
       fprintf(stderr, "Error: LaudConst initialization failed.\n");
-      exit(1);
+      abort();
     }
 
-    MinusOne = init(LaudConst, -1.);
-    One = init(LaudConst, 1.);
-    Zero = init(LaudConst, 0.);
+    // Initialize constant values: MinusOne, One, and Zero
+    float minus_one = -1.0;
+    MinusOne = laud_const(0, NULL, 1, &minus_one);
 
+    float one = 1.0;
+    One = laud_const(0, NULL, 1, &one);
+
+    float zero = 0.0;
+    Zero = laud_const(0, NULL, 1, &zero);
+
+    // Check if constant initialization was successful
     if (!MinusOne || !One || !Zero) {
       // Handle initialization failure.
       fprintf(stderr, "Error: Constant initialization failed.\n");
-      exit(1);
+      abort();
     }
   }
   return;
