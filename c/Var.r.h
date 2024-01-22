07ba6983d3f5a5947ee34f84bdfed2dc69bc1fba
iudah <olaolujude@gmail.com>
2024-01-22 14:59:28 +0100

    Refactor Var.r.h: Standardize naming conventions and improve comments
    This commit refactors Var.r.h to adhere to standardized naming
    conventions, ensuring consistency throughout the file. Additionally, it
    improves and clarifies comments for better code documentation.
    The changes include renaming the file to Var.r.h, standardizing the
    naming of structure members, and enhancing comments to provide clear
    explanations of the purpose and usage of certain functions and
    structures.

--- c/Var.r.h
+++ c/Var.r.h
@@ -1,14 +1,15 @@
 /**
- * @file Var.r.h
+ * @file Var_r.h
  *
  * This file defines the structure and declarations related to LaudVar objects.
  */
 #ifndef VAR_R_H
 #define VAR_R_H
 
-#include "../../Ubject/Ubject.r.h"
+#include "Placeholder.r.h"
 #include "ds/HashMap.h"
-#include "ds/Queue.h"
+
+#define LAUD_VAR_PRIORITY (UBJECT_PRIORITY + 2)
 
 /**
  * @struct LaudVar
@@ -17,12 +18,14 @@
  */
 struct LaudVar {
 #ifndef VAR_C
-  char ___[sizeof(struct __ {
+  /* Private members to simulate encapsulation */
+  char ___[sizeof(struct LaudVar_private {
 #endif
-    struct Ubject _; /**< Inherited from the Ubject class. */
-    float value;     /**< The float value stored in the LaudVar object. */
-    char discontinuity_marker : 1; /**< A marker indicating discontinuity (1 for
-                                      discontinuous, 0 for continuous). */
+    struct LaudPlaceholder _; /**< Parent class for LaudVar. */
+    float *values; /**< The float values stored in the LaudVar object. */
+    size_t *rank_shape_length; /**< Array holding rank, shape, and length. */
+    unsigned char is_continuous : 1; /**< Flag indicating continuity (1 for
+                                        continuous, 0 for discontinuous). */
 #ifndef VAR_C
   })];
 #endif
@@ -40,26 +43,10 @@ struct LaudVarClass {
   void (*differentiate)(
       const void *self, void *derivative,
       struct LaudHashMap *ddx); /**< A function pointer for differentiation. */
-  char (*isFloat)(const void *self); /**< A function pointer to check if LaudVar
-                                        holds a float value. */
+  char (*is_float)(const void *self); /**< A function pointer to check if
+                                         LaudVar holds a float value. */
 };
 
-/**
- * @brief Function to evaluate a LaudVar object.
- *
- * @param self A pointer to the LaudVar object to be evaluated.
- * @return A pointer to the result of the evaluation.
- */
-static void *LaudVar_evaluate(void *self);
-/**
- * @brief Function to compute the derivative of a LaudVar object.
- *
- * @param self A pointer to the LaudVar object.
- * @param derivative A pointer to the derivative object.
- * @param ddx An array of pointers to store the derivatives.
- */
-static void LaudVar_differentiate(const void *self, const void *derivative,
-                                  struct LaudHashMap *ddx);
 /**
  * @var LaudVarClass
  *
@@ -68,25 +55,36 @@ static void LaudVar_differentiate(const void *self, const void *derivative,
 extern const void *LaudVarClass;
 
 /**
- * @var LaudVarProtected
+ * @var laud_var_protected
  *
  * Structure containing function pointers for protected LaudVar functions.
  */
-extern const struct LaudVarProtected {
+extern const struct laud_var_protected {
   /**
    * @brief Function to set the float value of a LaudVar object.
    *
    * @param self A pointer to the LaudVar object.
+   * @param ndx The index to set the value.
    * @param value The float value to be set.
    * @return The updated float value.
    */
-  float (*setValue)(void *self, float value);
+  float (*set_value)(void *self, size_t ndx, float value);
+
   /**
    * @brief Function to get the float value stored in a LaudVar object.
    *
    * @param self A pointer to the LaudVar object.
-   * @return The float value stored in the LaudVar object.
+   * @param ndx The index to retrieve the value.
+   * @return The value at the specified index.
    */
-  float (*getValue)(const void *self);
-} LaudVarProtected;
+  float (*get_value)(const void *self, size_t ndx);
+} laud_var_protected;
+
+/**
+ * @brief Function to get a random float value.
+ *
+ * @return A random value.
+ */
+float laud_rng();
+
 #endif
