f6dcf3ef9e367e0b207f5a524b989738e52ee3c7
iudah <olaolujude@gmail.com>
2024-01-25 15:03:20 +0100

    Enhance LaudStack function declarations
    - Added missing include directive for <stddef.h>.
    - Extended LaudStackFn structure with functions for iteration and item
      replacement.
    - Updated function documentation to reflect the changes.
    - Aligned function declarations with their implementations in Stack.c.

--- c/ds/Stack.h
+++ c/ds/Stack.h
@@ -6,7 +6,8 @@
  */
 #ifndef STACK_H
 #define STACK_H
-#include <stdlib.h>
+
+#include <stddef.h>
 
 /**
  * @struct LaudStack
@@ -25,6 +26,7 @@ extern const struct LaudStackFn {
    * @return A pointer to the created LaudStack object.
    */
   void *(*Stack)(int count);
+
   /**
    * @brief Function to delete a LaudStack object.
    *
@@ -34,6 +36,7 @@ extern const struct LaudStackFn {
    * @param stack A pointer to the LaudStack to be deleted.
    */
   void (*del)(struct LaudStack *stack);
+
   /**
    * @brief Function to push an object onto the LaudStack.
    *
@@ -43,6 +46,7 @@ extern const struct LaudStackFn {
    * @param x A pointer to the object to be pushed.
    */
   void (*push)(struct LaudStack *stack, const void *x);
+
   /**
    * @brief Function to pop and remove an object from the LaudStack.
    *
@@ -53,6 +57,7 @@ extern const struct LaudStackFn {
    * @return A pointer to the popped object.
    */
   void *(*pop)(struct LaudStack *stack);
+
   /**
    * @brief Function to get the count of objects in the LaudStack.
    *
@@ -63,6 +68,7 @@ extern const struct LaudStackFn {
    * @return The count of objects in the stack.
    */
   int (*count)(const struct LaudStack *stack);
+
   /**
    * @brief Function to peek at an object in the LaudStack at a specific index.
    *
@@ -73,7 +79,45 @@ extern const struct LaudStackFn {
    * @param i The index of the object to peek at.
    * @return A pointer to the object at the specified index.
    */
-  void *(*peek)(const struct LaudStack *stack, int i);
+  void *(*peek)(const struct LaudStack *stack, size_t i);
+
+  /**
+   * @brief Function to begin the iteration over the LaudStack.
+   *
+   * @param stack A pointer to the LaudStack.
+   * @return The number of elements in the LaudStack.
+   */
+  size_t (*iter_start)(struct LaudStack *stack);
+
+  /**
+   * @brief Function to yield the next element during iteration.
+   *
+   * @param stack A pointer to the LaudStack.
+   * @return A pointer to the next element or NULL if the end of the LaudStack
+   * is reached.
+   */
+  void *(*yield)(struct LaudStack *stack);
+
+  /**
+   * @brief Function to end the iteration over the LaudStack.
+   *
+   * @param stack A pointer to the LaudStack.
+   */
+  void (*iter_end)(struct LaudStack *stack);
+
+  /**
+   * @brief Function to replace an item in the LaudStack at a specific index.
+   *
+   * This function replaces an item in the LaudStack at the specified index.
+   *
+   * @param stack A pointer to the LaudStack.
+   * @param item A pointer to the new item.
+   * @param array_index The index of the item to be replaced.
+   * @return A pointer to the item that was replaced or NULL if the index is
+   * out of bounds.
+   */
+  void *(*replace_item)(struct LaudStack *stack, void *item,
+                        size_t array_index);
 } LaudStackFn;
 
 #endif
