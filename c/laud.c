21dfe1e43384b85c7f629ff69664ec94d8d382e8
iudah <olaolujude@gmail.com>
2024-01-26 22:32:27 +0100

    Update tests in laud.c and main.cpp

--- c/laud.c
+++ c/laud.c
@@ -1,25 +1,45 @@
+#include <pthread.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include <Ubject.h>
+
 #include "Const.h"
 #include "Var.h"
-#include <stdio.h>
 
-int laud() {
-  void *a = LaudVarVar(3.1);
-  void *b = LaudConst_(3.9);
-  void *c = LaudVarProduct(a, b);
-  void *d = LaudVarProduct(c, c);
-  void *e = LaudVarProduct(d, c);
-  void *f = LaudVarProduct(e, c);
-  void *g = LaudVarProduct(f, c);
-  void *h = LaudVarProduct(g, c);
-  void *i = LaudVarProduct(h, c);
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+    int laud()
+{
+
+  printf("\n\n==============================================================\n"
+         "%s\n"
+         "==============================================================\n",
+         __FILE__);
+  float number = 3.1;
+  void *a = laud_var(0, NULL, 1, &number);
+  number = 3.9;
+  void *b = laud_const(0, NULL, 1, &number);
+  void *c = laud_product(a, b);
+  void *d = laud_product(c, c);
+  void *e = laud_product(d, c);
+  void *f = laud_product(e, c);
+  void *g = laud_product(f, c);
+  void *h = laud_product(g, c);
+  void *i = laud_product(h, c);
 
   {
-    void **ddx = LaudVarDerivativeMap();
-    LaudVarDifferentiate(d, NULL, ddx);
+    void **ddx = laud_create_derivative_map();
+    laud_differentiate(d, NULL, ddx);
 
-    LaudVarDerivativeMapIterStart(ddx);
+    laud_start_derivative_map_iteration(ddx);
     void **dx;
-    while ((dx = LaudVarDerivativeMapNext(ddx))) {
+    while ((dx = laud_yield_derivative_map_value(ddx)))
+    {
       printf("d\n\
 \n\
 x\n\
@@ -33,20 +53,21 @@ ref. count: %i\n\
 ptr: %p\n\
 -----------------------------\n\
 -----------------------------\n",
-             LaudVarGetFloatValue(dx[0]), getReference(dx[0]), dx[0],
-             LaudVarGetFloatValue(dx[1]), getReference(dx[1]), dx[1]);
+             laud_get_value(dx[0], 0), getReference(dx[0]), dx[0],
+             laud_get_value(dx[1], 0), getReference(dx[1]), dx[1]);
       blip(dx[1]);
     }
-    LaudVarDeleteDerivativeMap(ddx);
+    laud_delete_derivative_map(ddx);
   }
 
   {
-    void **ddx = LaudVarDerivativeMap();
-    LaudVarDifferentiate(g, NULL, ddx);
+    void **ddx = laud_create_derivative_map();
+    laud_differentiate(g, NULL, ddx);
 
-    LaudVarDerivativeMapIterStart(ddx);
+    laud_start_derivative_map_iteration(ddx);
     void **dx;
-    while ((dx = LaudVarDerivativeMapNext(ddx))) {
+    while ((dx = laud_yield_derivative_map_value(ddx)))
+    {
       printf("g\n\
 \n\
 x\n\
@@ -60,20 +81,21 @@ ref. count: %i\n\
 ptr: %p\n\
 -----------------------------\n\
 -----------------------------\n",
-             LaudVarGetFloatValue(dx[0]), getReference(dx[0]), dx[0],
-             LaudVarGetFloatValue(dx[1]), getReference(dx[1]), dx[1]);
+             laud_get_value(dx[0], 0), getReference(dx[0]), dx[0],
+             laud_get_value(dx[1], 0), getReference(dx[1]), dx[1]);
       blip(dx[1]);
     }
-    LaudVarDeleteDerivativeMap(ddx);
+    laud_delete_derivative_map(ddx);
   }
 
   {
-    void **ddx = LaudVarDerivativeMap();
-    LaudVarDifferentiate(i, NULL, ddx);
+    void **ddx = laud_create_derivative_map();
+    laud_differentiate(i, NULL, ddx);
 
-    LaudVarDerivativeMapIterStart(ddx);
+    laud_start_derivative_map_iteration(ddx);
     void **dx;
-    while ((dx = LaudVarDerivativeMapNext(ddx))) {
+    while ((dx = laud_yield_derivative_map_value(ddx)))
+    {
       printf("i\n\
 \n\
 x\n\
@@ -87,12 +109,23 @@ ref. count: %i\n\
 ptr: %p\n\
 -----------------------------\n\
 -----------------------------\n",
-             LaudVarGetFloatValue(dx[0]), getReference(dx[0]), dx[0],
-             LaudVarGetFloatValue(dx[1]), getReference(dx[1]), dx[1]);
+             laud_get_value(dx[0], 0), getReference(dx[0]), dx[0],
+             laud_get_value(dx[1], 0), getReference(dx[1]), dx[1]);
       blip(dx[1]);
     }
-    LaudVarDeleteDerivativeMap(ddx);
+    laud_delete_derivative_map(ddx);
   }
 
+  printf("------------------------------------------------------\n");
+
+  void *txt = laud_from_text(
+      "/data/data/com.termux/files/home/storage/shared/Jay/Projects/"
+      "laud/tmp.csv",
+      ", ");
+  char string[1024];
+  printf("%s\n", laud_as_string(txt, string, 1024));
+  laud_slice(txt, "2, :");
+
+  printf("%s\n", laud_as_string(laud_slice(txt, "2, :"), string, 1024));
   return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,33 +1,49 @@
 #include "cpp/Const.hpp"
 #include "cpp/Derivatives.hpp"
+#include <cstdio>
 #include <iostream>
 #include <vector>
 
-extern "C" int laud();
+extern "C"
+#ifdef _Win32
+    __declspec(dllimport)
+#endif
+        int laud();
 
-int main() {
+int main()
+{
+  printf("\n\n==============================================================\n"
+         "%s\n"
+         "==============================================================\n",
+         __FILE__);
+
   std::vector<Laud::Var> variables = {Laud::Const(3), Laud::Const(9.02),
                                       Laud::Var(4.5), Laud::Var(8.445)};
-  Laud::Var product = variables[0] * variables[1];
   Laud::Var product1 = variables[3] * variables[1];
+
+  Laud::Var product = variables[0] * variables[1];
   Laud::Var difference = product - variables[2];
   Laud::Var sum = difference + variables[3];
-  std::cout << (Laud::Const(90.9) / variables[2]) << " !\n";
+  std::cout << (Laud::Const(90.9) / variables[2]).evaluate() << "=90.9/4.5!\n";
 
   std::cout << "variables[0] (differentiation): "
             << variables[0].differentiate() << '\n';
 
-  std::cout << "variables[0] (differentiation): "
-            << variables[0].differentiate() << '\n';
+  std::cout << "variables[1] (differentiation): "
+            << variables[1].differentiate() << '\n';
 
   std::cout << "variables[3] (differentiation): "
             << variables[3].differentiate() << '\n';
 
-  std::cout << "product1 (differentiation): " << product1.differentiate()
-            << '\n';
-  std::cout << "sum (differentiation): " << sum.differentiate() << '\n';
+  std::cout << "product1 (differentiation): "
+            << product1.evaluate().differentiate() << '\n';
 
-  std::cout << "sum (differentiation): " << sum.differentiate() << '\n';
+  std::cout << "difference (differentiation): " << product << " - "
+            << variables[2] << " : " << difference.evaluate().differentiate()
+            << '\n';
+
+  std::cout << "sum (differentiation): " << difference << " - " << variables[3]
+            << " : " << sum.evaluate().differentiate() << '\n';
 
   return laud();
 }
