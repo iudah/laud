17d06cceab988e78dd45992b5da06d78e8497f82
iudah <olaolujude@gmail.com>
2024-05-23 09:35:11 +0100

    Refactor and enhance misc.c with fixed-width integer types and new functions
    - Added #include <stdint.h> for fixed-width integer types.
    - Updated function signatures to use uint64_t and uint16_t instead of size_t for consistency and clarity.
    - Removed unused functions laud_add and laud_matrix_dot.
    - Modified laud_to_string, laud_shape, and laud_rank to use fixed-width integer types.
    - Added new functions laud_differentiate and laud_reduce to support differentiation and reduction operations.
    - Updated corresponding function calls to ensure proper type handling and consistency across the codebase.

--- c/misc/misc.c
+++ c/misc/misc.c
@@ -1,40 +1,29 @@
 #include <Ubject.h>
+#include <stdint.h>
 
+#include "../core/base.h"
 #include "../core/base.r.h"
 
 #define MISC_STATIC
 
 #undef MISC_STATIC
 
-void *laud_add(void *a, void *b) {
-  const struct laud_base_class *class = classOf(a);
-  return class->add(a, b, NULL);
-}
-
-void *laud_matrix_dot(void *a, void *b) {
-  const struct laud_base_class *class = classOf(a);
-
-  UbjectError.warn("| - - %s - - |", className(a));
-
-  return class->matrix_dot(a, b, NULL);
-}
-
 char *laud_to_string(const void *laud_object, char *buffer,
-                     const size_t buffer_limit) {
+                     const uint64_t buffer_limit) {
 
   const struct laud_base_class *class = classOf(laud_object);
 
   return class->to_string(laud_object, buffer, buffer_limit);
 }
 
-const size_t *laud_shape(void *laud_object) {
+const uint64_t *laud_shape(void *laud_object) {
 
   const struct laud_base_class *class = classOf(laud_object);
 
   return class->shape(laud_object);
 }
 
-size_t laud_rank(void *laud_object) {
+uint16_t laud_rank(void *laud_object) {
 
   const struct laud_base_class *class = classOf(laud_object);
 
@@ -46,4 +35,20 @@ void laud_evaluate(void *laud_object) {
   const struct laud_base_class *class = classOf(laud_object);
 
   return class->evaluate(laud_object, NULL);
+}
+
+void laud_differentiate(void *laud_object, void *pre_derivatives) {
+
+  const struct laud_base_class *class = classOf(laud_object);
+
+  return class->differentiate(laud_object, pre_derivatives, NULL);
+}
+
+void *laud_reduce(void *operand, uint16_t axis,
+                  float (*callback)(const float current_net,
+                                    const float *const values,
+                                    const void *args),
+                  const void *args) {
+  const struct laud_base_class *class = classOf(operand);
+  return class->reduce(operand, axis, callback, args, NULL);
 }
