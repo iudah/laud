2dc0a72625c491adeb29540bcaa413f77a8afd2c
iudah <olaolujude@gmail.com>
2024-06-13 20:35:20 +0100

    Enhanced type safety and added new utility function in `misc.c`
    - Changed `laud_differentiate` to accept a `const void*` for `pre_derivatives`, ensuring the input is not modified.
    - Modified `laud_reduce` to use `number_t` instead of `float` for callback parameters, enhancing precision and consistency.
    - Added a new utility function `laud_value_at_offset` to retrieve a value from an operand at a given offset.

--- c/misc/misc.c
+++ c/misc/misc.c
@@ -37,7 +37,7 @@ void laud_evaluate(void *laud_object) {
   return class->evaluate(laud_object, NULL);
 }
 
-void laud_differentiate(void *laud_object, void *pre_derivatives) {
+void laud_differentiate(void *laud_object, const void *pre_derivatives) {
 
   const struct laud_base_class *class = classOf(laud_object);
 
@@ -45,10 +45,16 @@ void laud_differentiate(void *laud_object, void *pre_derivatives) {
 }
 
 void *laud_reduce(void *operand, uint16_t axis,
-                  float (*callback)(const float current_net,
-                                    const float *const values,
-                                    const void *args),
+                  number_t (*callback)(const number_t current_net,
+                                       const number_t *const values,
+                                       const void *args),
                   const void *args) {
   const struct laud_base_class *class = classOf(operand);
   return class->reduce(operand, axis, callback, args, NULL);
+}
+
+number_t laud_value_at_offset(void *operand, uint64_t offset) {
+
+  const struct laud_base_class *class = classOf(operand);
+  return class->value_at_offset(operand, offset, NULL);
 }
